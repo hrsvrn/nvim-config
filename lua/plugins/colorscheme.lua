c9e189c57302ea198526529bbedb1b1d01763f04
hrsvrn <harshvardhanvatsa@gmail.com>
2024-07-26 01:32:14 +0530

    Added more color configs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
   "maxmx03/solarized.nvim",
   "nyoom-engineering/oxocarbon.nvim",
   "Mofiqul/vscode.nvim",
+  "diegoulloao/neofusion.nvim",
+  "edmondburnett/leeward.nvim",
+  "oxfist/night-owl.nvim",
   lazy = true,
   priority = 1000,
   opts = {
